bd7f12462a7129ee4fe453f69dd470125de6ef39
Kenneth-Blanton <knsylvest@gmail.com>
2022-12-12 00:30:31 -0600

    h

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import HeadShot from './pics/IMG_0753.jpeg';
+
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <div className="mainRunner">
+                <img src={HeadShot} alt="headshot"/>
+                <div id="Bio">
+                Self taught chef specializing in <br/> Seasoned Cuisine and Stress Free Dining. <br/> Sit back and relax while Chef Moody handles <br/> your game night, girls night, and date night appetite. <br/> From finger foods to fine dining, all meals provide <br/> that homemade feel and a stress reliving mood.
+                </div>
+            </div>
         </div>
      );
 }
