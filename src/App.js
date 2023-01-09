ddc6a529ddf9520d5a2fdaffd98a18310475e953
Kenneth-Blanton <74569193+kenneth-blanton@users.noreply.github.com>
2023-01-09 14:20:26 -0600

    Connecting to GitHub

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
     <div className='App'>
       <NavBar></NavBar>
       <Routes>
-        <Route exact path='' element={<Home></Home>}></Route>
-        <Route exact path='/merch' element={<Merch></Merch>}></Route>
+        <Route exact path='/chef-kiss/' element={<Home></Home>}></Route>
+        <Route exact path='chef-kiss/merch' element={<Merch></Merch>}></Route>
         <Route path='chef-kiss/recipes' element={<Recipes></Recipes>}></Route>
         <Route exact path='chef-kiss/BuffaloWingsDownload' element={<BuffaloWingsDownload></BuffaloWingsDownload>}></Route>
         <Route exact path='chef-kiss/BlackenedSalmonDownload' element={<BlackenedSalmonDownload></BlackenedSalmonDownload>}></Route>
